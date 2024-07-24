e8e35519e347739cb0dd2c9a460b38d9a024df55
JJ <jonjones320@gmail.com>
2024-07-23 23:29:10 -0700

    Removed console.log's

--- Madlibs/src/Sentence.jsx
+++ Madlibs/src/Sentence.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 
 
 function Sentence({ words }) {
-    console.log("SENTENCE: ", words.adjective, words.noun, words.verb)
     return (
         <span className='Sentence-main'>
             The {words.adjective} {words.noun} {words.verb} over the small hill
         </span>
     );
 }
-
 
 
 export default Sentence;
