aa01a1e849073a323c1740b70f848f2eeaef7302
JJ <jonjones320@gmail.com>
2024-07-23 23:30:01 -0700

    Added a period to the sentence.

--- Madlibs/src/Sentence.jsx
+++ Madlibs/src/Sentence.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function Sentence({ words }) {
     return (
         <span className='Sentence-main'>
-            The {words.adjective} {words.noun} {words.verb} over the small hill
+            The {words.adjective} {words.noun} {words.verb} over the small hill.
         </span>
     );
 }
