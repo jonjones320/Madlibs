ee4a22c743fc8a173ce23a280f0550251897e40c
JJ <jonjones320@gmail.com>
2024-07-23 17:01:05 -0700

    Added structure for receiving form data and passing it to the sentence and then the story component.

--- Madlibs/src/Madlib.jsx
+++ Madlibs/src/Madlib.jsx
@@ -1,1 +1,24 @@
 import { useState } from 'react'
+import MadlibForm from './MadlibForm'
+import Sentence from './Sentence'
+import Story from './Story'
+
+function Madlib() {
+    const [words, setWords] = useState({});
+    const [story, setStory] = useState("");
+
+    function handleFormSubmit() {
+        setWords(formData);
+        setStory(<Sentence words={formData} />)
+    }
+
+    return (
+        <div>
+            <h1>Madlib!</h1>
+            <MadlibForm onFormSubmit={handleFormSubmit}/>
+            <Story story={story}/>
+        </div>
+    )
+};
+
+export default Madlib;
