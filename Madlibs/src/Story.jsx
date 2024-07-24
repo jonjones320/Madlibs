41fe66b7564d1543d29dc9bd520cf12641463b32
JJ <jonjones320@gmail.com>
2024-07-23 23:28:21 -0700

    Built Story component

--- Madlibs/src/Story.jsx
+++ Madlibs/src/Story.jsx
@@ -1,8 +1,17 @@
 import { useState } from 'react'
 
 
-function Story() {
-    return 
+function Story({ story }) {
+    return (
+        <div className='Story-main'>
+            <h3 className='Story-header'>
+                Your story: 
+            </h3>
+            <p className='Story-paragraph'>
+                { story }
+            </p>
+        </div>
+    )
 }
 
 
